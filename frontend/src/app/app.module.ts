9c50bd4806c87f34f06299bbd7a3df5c2b0b6020
rachitgupta98 <rachit371@gmail.com>
2020-12-30 19:28:32 +0530

    error removed

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './_components/login/login.component';
 import { SampleComponent } from './_components/sample/sample.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     SampleComponent
   ],
   imports: [
