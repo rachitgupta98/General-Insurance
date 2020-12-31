32fcebd529359cb3b30cdb0c730afc029dceaaa2
rachitgupta98 <rachit371@gmail.com>
2020-12-31 21:49:39 +0530

    import Material module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SampleComponent } from './_components/sample/sample.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './_modules/material/material.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
