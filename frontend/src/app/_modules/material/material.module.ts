733e07b7cd72abaca4848796395c03a345d53697
rachitgupta98 <rachit371@gmail.com>
2021-01-02 14:20:00 +0530

    add material ui module

--- frontend/src/app/_modules/material/material.module.ts
+++ frontend/src/app/_modules/material/material.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import {MatButtonModule} from "@angular/material";
 
-
+const MaterialComponents=[
+  MatButtonModule
+]
 @NgModule({
 
-  imports: [MatButtonModule],
-  exports:[MatButtonModule]
+  imports: [MaterialComponents],
+  exports:[MaterialComponents]
 })
 export class MaterialModule { }
