e201a906285c94f721334b40fc7724a0371f6e6c
GvsSaiSumanth <gvssaisumanth24@gmail.com>
2021-01-02 14:22:18 +0530

    added user Registration

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SampleComponent } from './_components/sample/sample.component';
+import { UserRegistrationComponent } from './user-registration/user-registration.component';
+import { FormsModule } from '@angular/forms';
+import { MaterialUIModule } from './material-ui/material-ui.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SampleComponent
+    SampleComponent,
+    UserRegistrationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MaterialUIModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
