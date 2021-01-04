bad45580bd3607a0544f6f22bc8de081aeb8a14e
nidhi jadhav <nidijadhav2164@gmail.com>
2021-01-04 18:34:48 +0530

    addedlogin

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,15 +5,22 @@ import { AppComponent } from './app.component';
 import { SampleComponent } from './_components/sample/sample.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './_modules/material/material.module';
+import { LoginComponent } from './_components/login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AboutUsComponent } from './_components/about-us/about-us.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    SampleComponent
+    SampleComponent,
+    LoginComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
     MaterialModule
   ],
   providers: [],
