4464035ae79198911486b47e384106efdc628fa2
rachitgupta98 <rachit371@gmail.com>
2021-01-07 23:00:53 +0530

    routing updated

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,18 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Routes, RouterModule } from '@angular/router';
-import { UserRegistrationComponent } from './_components/user-registration/user-registration.component';
-import { HomeComponent } from './_components/home/home.component';
+import { LoginComponent } from "./_components/login/login.component";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Routes, RouterModule } from "@angular/router";
+import { UserRegistrationComponent } from "./_components/user-registration/user-registration.component";
+import { HomeComponent } from "./_components/home/home.component";
+import { VehicleHomeComponent } from "./_components/vehicle-home/vehicle-home.component";
 
 const routes: Routes = [
-  {path:'userRegistration',component:UserRegistrationComponent},
-  { path: 'home', component: HomeComponent},
-  // { path: '', redirectTo: '/home', pathMatch: 'full' }
-]
+  { path: "userRegistration", component: UserRegistrationComponent },
+  { path: "home", component: VehicleHomeComponent },
+  { path: "login", component: LoginComponent },
+  //{ path: "**", redirectTo: "home" },
+];
 
 @NgModule({
   declarations: [],
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
