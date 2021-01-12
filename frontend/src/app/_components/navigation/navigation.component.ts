a67bb41e747d257d77917c15df114862e36f828d
rachitgupta98 <rachit371@gmail.com>
2021-01-12 18:37:19 +0530

    components updated

--- frontend/src/app/_components/navigation/navigation.component.ts
+++ frontend/src/app/_components/navigation/navigation.component.ts
@@ -1,21 +1,22 @@
-import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { Component } from "@angular/core";
+import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
+import { Observable } from "rxjs";
+import { map, shareReplay } from "rxjs/operators";
 
 @Component({
-  selector: 'app-navigation',
-  templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss']
+  selector: "app-navigation",
+  templateUrl: "./navigation.component.html",
+  styleUrls: ["./navigation.component.scss"],
 })
 export class NavigationComponent {
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(map((result) => result.matches));
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-     
-    );
-
-  constructor(private breakpointObserver: BreakpointObserver) {}
-
+  constructor(private breakpointObserver: BreakpointObserver) {
+    console.log(sessionStorage.getItem("regNo"));
+  }
+  ngDoCheck() {
+    console.log(sessionStorage.getItem("regNo"));
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -19,6 +19,7 @@ import { HomeComponent } from "./_components/home/home.component";
 import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from "./app-routing.module";
 import { NavigationComponent } from "./_components/navigation/navigation.component";
+import { PolicyFormComponent } from './_components/policy-form/policy-form.component';
 
 @NgModule({
   declarations: [
@@ -32,6 +33,7 @@ import { NavigationComponent } from "./_components/navigation/navigation.compone
     InputFieldComponent,
     LoginComponent,
     AboutUsComponent,
+    PolicyFormComponent,
   ],
   imports: [
     BrowserModule,
