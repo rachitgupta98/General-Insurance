5b3c4d222ce03889fe69c71efcb7cfc2bc77a02f
rachitgupta98 <rachit371@gmail.com>
2021-01-13 17:49:16 +0530

    added policy display

---
+++ frontend/src/app/_components/policy-display/policy-display.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PolicyDisplayComponent } from './policy-display.component';
+
+describe('PolicyDisplayComponent', () => {
+  let component: PolicyDisplayComponent;
+  let fixture: ComponentFixture<PolicyDisplayComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PolicyDisplayComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PolicyDisplayComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
